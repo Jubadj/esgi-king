ff281f6299775ae1e664337b138b613e9713bf75
Juba <adjaoud.juba.17@gmail.com>
2022-05-24 14:18:02 +0200

    Readme updated

--- controllers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './auth.controller';
-//export * from './bigBoss.controller';
-export * from './restaurant.controller';
-export * from './product.controller';
-export * from './discount.controller';
